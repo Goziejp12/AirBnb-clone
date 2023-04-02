0dd53bd7fdf2e98569a2cf0ff276278714a9ecf5
Chigozie Johnpaul Maduka <chigoziemaduka401@gmail.com>
2023-04-02 01:01:05 +0100

    final fix

--- src/data.js
+++ src/data.js
@@ -91,7 +91,7 @@ export default [
         openSpots: 0,
     },
     {
-    id: 8,
+        id: 8,
         title: "Exciting 'Movie Trivia Night' for happy team connections",
         description: "Exciting 'Movie Trivia Night' for happy team connections",
         price: 25,
@@ -102,5 +102,83 @@ export default [
         },
         location: "India",
         openSpots: 100,
+    },
+    {
+        id: 9,
+        title: "Creative Boosters - Creative icebreakers for teams",
+        description: "Creative Boosters - Creative icebreakers for teams",
+        price: 12,
+        coverImg: "airbnb6.webp",
+        stats: {
+            rating: 5.0,
+            reviewCount: 18
+        },
+        location: "Poland",
+        openSpots: 0,
+    },
+    {
+        id: 10,
+        title: "Tiramisu, ice cream & sorbet, all secrets",
+        description: "Tiramisu, ice cream & sorbet, all secrets",
+        price: 33,
+        coverImg: "airbnb7.webp",
+        stats: {
+            rating: 4.97,
+            reviewCount: 148
+        },
+        location: "Italy",
+        openSpots: 10,
+    },
+    {
+        id: 11,
+        title: "Chanting meditation with a japanese monk",
+        description: "Chanting meditation with a japanese monk",
+        price: 16,
+        coverImg: "airbnb8.webp",
+        stats: {
+            rating: 4.94,
+            reviewCount: 1944
+        },
+        location: "Online",
+        openSpots: 130,
+    },
+    {
+        id: 12,
+        title: "Live Tulum like a local",
+        description: "Live Tulum like a local",
+        price: 20,
+        coverImg: "airbnb9.webp",
+        stats: {
+            rating: 4.69,
+            reviewCount: 13
+        },
+        location: "Mexico",
+        openSpots: 130,
+    },
+    {
+        id: 13,
+        title: "Guided JFK Assassination Walking Tour",
+        description: "Guided JFK Assassination Walking Tour",
+        price: 29,
+        coverImg: "airbnb10.webp",
+        stats: {
+            rating: 4.89,
+            reviewCount: 139
+        },
+        location: "Online",
+        openSpots: 1,
+    },
+    {
+        id: 14,
+        title: "Down Dallas History and Sightseeing Adventure",
+        description: "Down Dallas History and Sightseeing Adventure",
+        price: 69,
+        coverImg: "airbnb11.webp",
+        stats: {
+            rating: 4.84,
+            reviewCount: 25
+        },
+        location: "Online",
+        openSpots: 11,
     },
 ]
